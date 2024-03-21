9e32847a19836d961322620e068b463969712729
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-03-21 15:25:17 +0330

    feat(form):add form itmes to Form component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -14,14 +20,43 @@ function Logo() {
 }
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>what do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((mapItem) => (
+          <option value={mapItem} key={mapItem}>
+            {mapItem}
+          </option>
+        ))}
+      </select>
+      <input placeholder="item..." type="text" />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item itemObj={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ itemObj }) {
+  return (
+    <li>
+      <span style={itemObj.packed ? { textDecoration: "line-through" } : {}}>
+        {itemObj.quantity} {itemObj.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
