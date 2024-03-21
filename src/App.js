de88280b5c777719429f785defc93df1cf76638f
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-03-21 14:37:15 +0330

    feat(App.js):add main components to the App.js file the main components is: App, Logo, Form, PackingList, Stats

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
 export default function App() {
-  return <div>hello react</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>✈Far Away👜</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      👜 you have x items on your list, and already x
+    </footer>
+  );
 }
